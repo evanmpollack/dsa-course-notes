b71582d2ef5e64663e93c480b9ad68717cd13902
evanmpollack <evanmpollack@gmail.com>
2024-10-02 20:26:05 -0400

    adjacency list/matrix dfs/bfs

--- graph/Graph.java
+++ graph/Graph.java
@@ -15,11 +15,33 @@ public class Graph {
         one.addNeighbor(two);
         start.addNeighbor(one);
         start.addNeighbor(four);
+
+        Map<Vertex, List<Vertex>> adjList = createAdjList(List.of(start, one, two, three, four));
+
+        // Same graph created with vertex class
+        int[][] adjMatrix = new int[][] {
+            { 0, 1, 0, 0, 1 },
+            // directed so there is technicaly no edge at (1, 0) even though (0, 1) exists
+            { 0, 0, 1, 0, 0 },
+            { 0, 0, 0, 1, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 1, 0 }
+        };
+
         for (int i = 0; i < 6; i++) {
-            System.out.println("DFS (target=" + i + "): " + dfs(start, new HashSet<>(), i));
-            System.out.println("BFS (target=" + i + "): " + bfs(start, i));
+            System.out.println("DFS Class (target=" + i + "): " + dfs(start, new HashSet<>(), i));
+            System.out.println("BFS Class (target=" + i + "): " + bfs(start, i));
+            System.out.println("DFS List (target=" + i + "): " + dfs(start, adjList, new HashSet<>(), i));
+            System.out.println("BFS List (target=" + i + "): " + bfs(start, adjList, i));
+            System.out.println("DFS Matrix (target=" + i + "): " + dfs(0, adjMatrix, new HashSet<>(), i));
+            System.out.println("BFS Matrix (target=" + i + "): " + bfs(0, adjMatrix, i));
         }
     }
+
+    // ---------------------------------------------------------------------------------------------------
+
+    // Personal Implementations of BFS/DFS on Object-based, adjacency list, and adjacency matrix
+
 
     // Unweighted Object-based Graph DFS
     private static boolean dfs(Vertex curr, Set<Vertex> visited, int target) {
@@ -38,6 +60,51 @@ public class Graph {
         for (Edge e : curr.getNeighbors()) {
             // short circuits if found is true, preventing unneccesary recursive calls
             found = found || dfs(e.getDestination(), visited, target);
+        }
+
+        return found;
+    }
+
+    // Adjacency List - Using vertex class is a bit redundant because the class already has a list of edges in which verticies can be accessed
+    // However, this version also works for graphs that don't have associated Vertex/Edge classes
+    private static boolean dfs(Vertex curr, Map<Vertex, List<Vertex>> adjList, Set<Vertex> visited, int target) {
+        if (curr == null || visited.contains(curr)) {
+            return false;
+        }
+
+        visited.add(curr);
+        
+        if (curr.getData() == target) {
+            return true;
+        }
+
+        boolean found = false;
+        for (Vertex v : adjList.get(curr)) {
+            found = found || dfs(v, adjList, visited, target);
+        }
+        return found;
+    }
+
+    // Adjacency Matrix DFS
+    private static boolean dfs(int curr, int[][] adjMatrix, Set<Integer> visited, int target) {
+        int[] edges = adjMatrix[curr];
+        boolean found = false;
+
+        if (visited.contains(curr)) {
+            return false;
+        }
+
+        visited.add(curr);
+
+        if (curr == target) {
+            return true;
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            // We only want to check edges that exist
+            if (edges[i] != 0) {
+                found = found || dfs(i, adjMatrix, visited, target);
+            }
         }
 
         return found;
@@ -82,4 +149,76 @@ public class Graph {
         
         return found;
     }
+
+    // Adjacency List BFS
+    private static boolean bfs(Vertex start, Map<Vertex, List<Vertex>> adjList, int target) {
+        Set<Vertex> visited = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+        boolean found = false;
+
+        if (start != null) {
+            queue.add(start);
+        }
+
+        while (!queue.isEmpty()) {
+            Vertex curr = queue.remove();
+            visited.add(curr);
+
+            if (curr.getData() == target) {
+                found = true;
+                break;
+            }
+
+            for (Vertex v : adjList.get(curr)) {
+                if (!visited.contains(v)) {
+                    queue.add(v);
+                }
+            }
+        }
+        return found;
+    }
+
+    // Adjacency Matrix BFS
+    private static boolean bfs(int start, int[][] graph, int target) {
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        boolean found = false;
+
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int curr = queue.remove();
+            visited.add(curr);
+
+            if (curr == target) {
+                found = true;
+                break;
+            }
+
+            for (int i = 0; i < graph[curr].length; i++) {
+                if (graph[curr][i] != 0 && !visited.contains(i)) {
+                    queue.add(i);
+                }
+            }
+        }
+        return found;
+    }
+
+    // ---------------------------------------------------------------------------------------------------
+
+    private static Map<Vertex, List<Vertex>> createAdjList(List<Vertex> nodes) {
+        Map<Vertex, List<Vertex>> adjList = new HashMap<>();
+        for (Vertex v : nodes) {
+            List<Vertex> neighbors = new ArrayList<>();
+            for (Edge e : v.getNeighbors()) {
+                neighbors.add(e.getDestination());
+            }
+            adjList.put(v, neighbors);
+        }
+        return adjList;
+    }
+
+    // ---------------------------------------------------------------------------------------------------
+
+    // In Class Implementations: 
 }
